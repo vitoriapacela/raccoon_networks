83b5879bf7e8656dd00a52f330dfe7393f0b502f
vitoriapacela <vitoriapacela@gmail.com>
2020-02-20 14:55:26 +0200

    plotly jupyter lab

--- conda_env.sh
+++ conda_env.sh
@@ -12,5 +12,28 @@ conda install -c conda-forge glob2 -y
 conda install -c anaconda networkx -y
 conda install -c plotly plotly -y
 
+# the following is needed to display plotly plots in jupyter lab
+# source: https://github.com/plotly/plotly.py#jupyterlab-support-python-35
+conda install -c conda-forge nodejs
+# Avoid "JavaScript heap out of memory" errors during extension installation
+# (OS X/Linux)
+export NODE_OPTIONS=--max-old-space-size=4096
+
+# Jupyter widgets extension
+jupyter labextension install @jupyter-widgets/jupyterlab-manager@1.1 --no-build
+
+# FigureWidget support
+jupyter labextension install plotlywidget@1.5.0 --no-build
+
+# and jupyterlab renderer support
+jupyter labextension install jupyterlab-plotly@1.5.0 --no-build
+
+# Build extensions (must be done to activate extensions since --no-build is used above)
+jupyter lab build
+
+# Unset NODE_OPTIONS environment variable
+# (OS X/Linux)
+unset NODE_OPTIONS
+
 conda update --all
 conda clean --all
